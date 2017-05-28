aa324c5e2f0b08f93ac104c0e38f39892ec9b452
Dmytro <dikape03@gmail.com>
2017-05-28 18:45:33 +0300

    Added region points to admin

--- system/admin.py
+++ system/admin.py
@@ -3,10 +3,21 @@ from django.contrib.auth.models import Group
 
 from rest_framework.authtoken.models import Token
 
-from .models import Region, PeopleCategory, People
+from .models import Region, PeopleCategory, People, RegionMapPoint
+
+
+class RegionPointInline(admin.TabularInline):
+    model = RegionMapPoint
+    extra = 1
+    min_num = 0
+
+
+class RegionAdmin(admin.ModelAdmin):
+    inlines = (RegionPointInline)
+
 
 admin.site.unregister(Token)
 admin.site.unregister(Group)
-admin.site.register(Region)
+admin.site.register(Region, RegionAdmin)
 admin.site.register(PeopleCategory)
 admin.site.register(People)
